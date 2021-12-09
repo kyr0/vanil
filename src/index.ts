52ca15ec840e2e197d681bb0e67289f76112da58
Aron Homberg <info@aron-homberg.de>
2021-12-09 15:53:43 +0100

    fix: ts-node call in CLI via npm script

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ const path = require('path')
 
 const argLine = [...process.argv].splice(2 /* remove the first two (node cmd, vanil bin) */).join(' ')
 
-const nodeModulesDir = path.resolve(__dirname, '../../../')
+// const nodeModulesDir = path.resolve(__dirname, '../../../')
 const vanilRootDir = path.resolve(__dirname, '../../')
-const tsNodeBin = path.join(nodeModulesDir, '.bin', 'ts-node')
-const tsVanilBin = path.join(vanilRootDir, 'src', 'vanil.ts')
 
-child_process.execSync(`${tsNodeBin} ${tsVanilBin} ${argLine}`, { stdio: 'inherit' })
+// const tsNodeBin = path.join(nodeModulesDir, '.bin', 'ts-node')
+// const tsVanilBin = path.join(vanilRootDir, 'src', 'vanil.ts')
+
+child_process.execSync(`npm run start -- ${argLine}`, { stdio: 'inherit', cwd: vanilRootDir })

11758cedeff60423809f7d1a0f6ad145b6c57ef6
Aron Homberg <info@aron-homberg.de>
2021-12-09 20:39:21 +0100

    fix: cwd relative scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 'use strict'
+
+import { resolve } from 'path/posix'
+
 const child_process = require('child_process')
 const path = require('path')
 
@@ -7,9 +10,14 @@ const yargs = require('yargs-parser')
 const flags = yargs(process.argv)
 const cmd = flags._[2]
 
-if (cmd === 'init' && flags._[3]) {
+if (cmd === 'init') {
   console.log('init -> cwd path empty, defaulting to', process.cwd())
-  flags._[3] = process.cwd()
+
+  if (flags._[3]) {
+    path.resolve(process.cwd(), flags._[3])
+  } else {
+    flags._[3] = process.cwd()
+  }
 }
 
 const argLine = [...process.argv].splice(2 /* remove the first two (node cmd, vanil bin) */).join(' ')

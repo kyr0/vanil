15b6ce9b8428c9f930a08efbe3b90232ebbe1a5b
Aron Homberg <info@aron-homberg.de>
2021-12-09 21:14:59 +0100

    fix: cwd relative scaffolding

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,23 @@ const yargs = require('yargs-parser')
 const flags = yargs(process.argv)
 const cmd = flags._[2]
 
-let prefix = ''
+const vanilRootDir = path.resolve(__dirname, '../../')
+const argLine = [...process.argv].splice(2 /* remove the first two (node cmd, vanil bin) */).join(' ')
 
 // CWD we're in atm will be chdir to package/module location dir
 // when npm is called to run ts-node, therefore target directory
 // argument needs to be resolved (only relevant for npx vanil init use-case)
 if (cmd === 'init') {
-  prefix = `--prefix ${path.resolve(__dirname, '../../')}`
   if (flags._[3]) {
     process.argv[3] = path.resolve(process.cwd(), flags._[3])
   } else {
     process.argv[3] = process.cwd()
   }
+  child_process.execSync(`npm --prefix ${vanilRootDir} run start -- ${argLine}`, { stdio: 'inherit' })
+} else {
+  const nodeModulesDir = path.resolve(__dirname, '../../../')
+  const tsNodeBin = path.join(nodeModulesDir, '.bin', 'ts-node')
+  const tsVanilBin = path.join(vanilRootDir, 'src', 'vanil.ts')
+
+  child_process.execSync(`${tsNodeBin} ${tsVanilBin} ${argLine}`, { stdio: 'inherit' })
 }
-
-const argLine = [...process.argv].splice(2 /* remove the first two (node cmd, vanil bin) */).join(' ')
-
-child_process.execSync(`npm ${prefix} run start -- ${argLine}`, { stdio: 'inherit' })
